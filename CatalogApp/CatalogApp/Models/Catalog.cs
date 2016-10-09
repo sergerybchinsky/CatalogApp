96a398184e650dc9ecc61a165f4571ae9d047ed6
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-10-10 00:15:01 +0700

    Updated models for SQLite.net.

--- CatalogApp/CatalogApp/Models/Catalog.cs
+++ CatalogApp/CatalogApp/Models/Catalog.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
 
 namespace CatalogApp
 {
 	[DataContract]
 	public class Catalog
 	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
 		[DataMember(Name="title")]
 		public string Title { get; set; }
+
+		[OneToMany(CascadeOperations = CascadeOperation.All)]      // One to many relationship with Valuation
 		[DataMember(Name = "subs")]
 		public Subject[] Subjects { get; set; }
 	}
--- CatalogApp/CatalogApp/Models/Subject.cs
+++ CatalogApp/CatalogApp/Models/Subject.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Runtime.Serialization;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
 
 namespace CatalogApp
 {
 	[DataContract]
 	public class Subject
 	{
+		[PrimaryKey]
 		[DataMember(Name = "id")]
 		public int ID { get; set; }
+
+		[ForeignKey(typeof(Catalog))]     // Specify the foreign key
+		public int CatalogID { get; set; }
+
 		[DataMember(Name = "title")]
 		public string Title { get; set; }
+
+		[ManyToOne]
+		public Catalog Catalog { get; set; }
 	}
 }
