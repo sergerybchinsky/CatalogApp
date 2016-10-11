144f0be4bf9552ed411d59cb4ce7306d5171df1f
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-10-11 23:55:19 +0700

    Added reload DB bar item.

--- CatalogApp/iOS/Views/CategoriesView.cs
+++ CatalogApp/iOS/Views/CategoriesView.cs
@@ -69,15 +69,24 @@ namespace CatalogApp.iOS.Views
 		{
 			base.ViewDidLoad();
 			Title = "Catalog";
+
+			UIApplication.SharedApplication.KeyWindow.RootViewController = this.NavigationController;
+
 			var dataSource = new TableViewDataSource<Category>(this.TableView);
 			this.TableView.RegisterClassForCellReuse(typeof(UITableViewCell), "UITableViewCell");
 			this.TableView.Delegate = dataSource;
 			this.TableView.DataSource = dataSource;
 
+			var reloadDbButton = new UIBarButtonItem();
+			reloadDbButton.Image = UIImage.FromBundle(Const.ImageResource.WebIconImage);
+			this.NavigationItem.RightBarButtonItem = reloadDbButton;
+
 			var set = this.CreateBindingSet<CategoriesView, CategoriesViewModel>();
 			set.Bind(dataSource).For(ds => ds.ItemsSource).To(vm => vm.Categories);
 			set.Bind(dataSource).For(ds => ds.ShowCategoryCommand).To(vm => vm.ShowSubjectCommand);
+			set.Bind(reloadDbButton).For(bt => bt).To(vm => vm.ShowLoadDataCommand);
 			set.Apply();
 		}
+
 	}
 }
