72177637aac678172efa1af8dd53cdef27dd0220
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-10-11 23:56:03 +0700

    Added condition for start view model.

--- CatalogApp/CatalogApp/App.cs
+++ CatalogApp/CatalogApp/App.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using CatalogApp.Helpers;
 using MvvmCross.Platform.IoC;
 
 namespace CatalogApp
 {
-  public class App : MvvmCross.Core.ViewModels.MvxApplication
-    {
-        public override void Initialize()
-        {
-            CreatableTypes()
-                .EndingWith("Service")
-                .AsInterfaces()
-                .RegisterAsLazySingleton();
-			RegisterAppStart<ViewModels.LoadDataViewModel>();
-        }
-    }
+	public class App : MvvmCross.Core.ViewModels.MvxApplication
+	{
+		public override void Initialize()
+		{
+			CreatableTypes()
+				.EndingWith("Service")
+				.AsInterfaces()
+				.RegisterAsLazySingleton();
 
+			if (Settings.FirstStartApplication)
+				RegisterAppStart<ViewModels.LoadDataViewModel>();
+			else
+				RegisterAppStart<ViewModels.CategoriesViewModel>();
+		}
+	}
 }
